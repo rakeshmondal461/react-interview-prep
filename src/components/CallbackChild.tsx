95ed9361dbb9f592616bb79466c00547b621d3ba
Rakesh <mondalrakesh461@gmail.com>
2025-05-08 23:10:44 +0530

    function type added

--- src/components/CallbackChild.tsx
+++ src/components/CallbackChild.tsx
@@ -1,13 +1,15 @@
 import React from "react";
-// It's a higher-order component that memoizes a functional component — 
+// It's a higher-order component that memoizes a functional component —
 // meaning it will skip re-rendering if the props haven't changed.
-const CallbackChild = React.memo(({ handleChildClick }: any) => {
-  console.log("Child rendered");
-  return (
-    <div>
-      <button onClick={handleChildClick}> Click from child</button>
-    </div>
-  );
-});
+const CallbackChild = React.memo(
+  ({ handleChildClick }: { handleChildClick: () => void }) => {
+    console.log("Child rendered");
+    return (
+      <div>
+        <button onClick={handleChildClick}> Click from child</button>
+      </div>
+    );
+  }
+);
 
 export default CallbackChild;
